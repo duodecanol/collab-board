7885d36474cad9c9c42c0ed14ba7599fcc766a26
HOME-2700 <metalhwal@gmail.com>
2022-01-22 01:30:14 +0900

    Icons componentization

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
-import BoardFrame from './components/boardframe/BoardFrame';
-import Ribbon from './components/ribbon/Ribbon';
-import ToolBox from './components/toolbox/ToolBox';
+import BoardFrame from './components/boardframe/BoardFrame'
+import Ribbon from './components/ribbon/Ribbon'
+import ToolBox from './components/toolbox/ToolBox'
 
 function App() {
   return (
-<>
-<Ribbon />
-<ToolBox />
-<BoardFrame />
-</>
-  );
+    <>
+      <Ribbon />
+      <ToolBox />
+      <BoardFrame />
+    </>
+  )
 }
 
-export default App;
+export default App
